09c80c944591ca43c157c6f704bba3cb29a04581
xiampb <ilhamadityapb@gmail.com>
2025-03-16 21:58:37 +0700

    add empty assets/images folder

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'homepage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try{
+    await Firebase.initializeApp();
+  } catch (e){
+    print('Error initializing Firebase: $e');
+  }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
